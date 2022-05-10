6351cfd07562a79223753cc4b15e238104c586d9
popolam <hany961031@naver.com>
2022-05-10 23:30:35 +0900

    2022-05-10

---
+++ src/baekjoon/bk_1700_멀티탭스케줄링.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class bk_1700_멀티탭스케줄링 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		
+	}
+}
